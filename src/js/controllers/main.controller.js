7c87720c1ab69a84cd223ded0c43268dcf8b4876
Robby Helms <robert.helms1@gmail.com>
2017-02-18 11:07:29 -0500

    Initial test step inputs

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,5 +1,23 @@
 angular.module('nightwatchUI').controller('mainCtrl', function(MainService, $state, $scope) {
   var self = this;
+  // var allOptions = MainService.getOptions();
 
-  self.testingVariable = "Main View";
+  self.testingVariable = "Current Test";
+
+  // self.nodeOptions = allOptions[self.flowName] ? ( allOptions[self.flowName][self.nodeName] ? allOptions[self.flowName][self.nodeName] : noResultString) : noResultString;
+  self.testSteps = [];
+  self.optionValue = "Possible Values";
+  self.optionValues = [
+    'Yes',
+    'No'
+  ];
+
+  self.addStep = function() {
+    var currentFlow = self.flowName;
+    var currentNode = self.nodeName;
+    var currentOption = self.nodeOption;
+    var currentValue = self.optionValue;
+    var newStep = currentFlow + " " + currentNode + " " + currentOption + " " + currentValue;
+    self.testSteps.push(newStep);
+  }
 });
--- src/js/services/mainService.service.js
+++ src/js/services/mainService.service.js
@@ -1,5 +1,19 @@
-(function() {
-  angular.module('nightwatchUI').service('MainService', function(localStorageService) {
+angular.module('nightwatchUI').service('MainService', function(localStorageService) {
 
-  });
-})();
+  var options = {
+    'flow' : {
+      'node' : {
+        'option1' : 'Select1',
+        'option2' : 'Select2'
+      }
+    }
+  };
+
+  function getOptions () {
+    return options;
+  }
+
+  return {
+    getOptions: getOptions
+  };
+});
